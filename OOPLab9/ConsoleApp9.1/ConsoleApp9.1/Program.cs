d0073a0bbae9e6ad6426efadc4eb12730d73351e
Khrystyna24 <156518876+Khrystyna24@users.noreply.github.com>
2024-12-09 23:43:34 +0200

    Add files via upload

---
+++ OOPLab9/ConsoleApp9.1/ConsoleApp9.1/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+
+interface ITaxCalculator
+{
+    double CalculateTax();
+}
+
+class LandTax : ITaxCalculator
+{
+    public double Area { get; set; } 
+    public double Rate { get; set; } 
+
+    public LandTax(double area, double rate)
+    {
+        Area = area;
+        Rate = rate;
+    }
+
+    public double CalculateTax()
+    {
+        return Area * Rate;
+    }
+}
+
+class CarTax : ITaxCalculator
+{
+    public double EngineCapacity { get; set; } 
+    public double RatePerLitre { get; set; }  
+
+    public CarTax(double engineCapacity, double ratePerLitre)
+    {
+        EngineCapacity = engineCapacity;
+        RatePerLitre = ratePerLitre;
+    }
+
+    public double CalculateTax()
+    {
+        return EngineCapacity * RatePerLitre;
+    }
+}
+
+class IncomeTax : ITaxCalculator
+{
+    public double Income { get; set; } 
+    public double TaxRate { get; set; } 
+
+    public IncomeTax(double income, double taxRate)
+    {
+        Income = income;
+        TaxRate = taxRate;
+    }
+
+    public double CalculateTax()
+    {
+        return Income * TaxRate / 100;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        ITaxCalculator landTax = new LandTax(500, 2.5);
+        ITaxCalculator carTax = new CarTax(2.0, 300);
+        ITaxCalculator incomeTax = new IncomeTax(5000, 18);
+
+        Console.WriteLine($"Земельний податок: {landTax.CalculateTax()} UAH");
+        Console.WriteLine($"Податок на автомобіль: {carTax.CalculateTax()} UAH");
+        Console.WriteLine($"Податок на прибуток: {incomeTax.CalculateTax()} UAH");
+        Console.WriteLine("Натисніть будь-яку клавішу, щоб закрити програму...");
+        Console.ReadKey();
+    }
+}
---
+++ OOPLab9/ConsoleApp9.2/ConsoleApp9.2/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+class Product : IComparable<Product>
+{
+    public string Name { get; set; }
+    public double Weight { get; set; }
+    public double Price { get; set; }
+    public int Quality { get; set; }
+
+    public Product(string name, double weight, double price, int quality)
+    {
+        Name = name;
+        Weight = weight;
+        Price = price;
+        Quality = quality;
+    }
+
+    public int CompareTo(Product other)
+    {
+        return Weight.CompareTo(other.Weight);
+    }
+
+    public override string ToString()
+    {
+        return $"{Name}: Вага={Weight}, Ціна={Price}, Якість={Quality}";
+    }
+}
+
+class ProductPriceComparer : IComparer<Product>
+{
+    public int Compare(Product x, Product y)
+    {
+        return x.Price.CompareTo(y.Price);
+    }
+}
+
+class ProductQualityComparer : IComparer<Product>
+{
+    public int Compare(Product x, Product y)
+    {
+        return x.Quality.CompareTo(y.Quality);
+    }
+}
+
+class ProductCollection : IEnumerable<Product>
+{
+    private List<Product> products = new List<Product>();
+
+    public void Add(Product product)
+    {
+        products.Add(product);
+    }
+
+    public IEnumerator<Product> GetEnumerator()
+    {
+        products.Sort(new ProductPriceComparer()); 
+        return products.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Console.OutputEncoding = System.Text.Encoding.UTF8;
+        var products = new ProductCollection();
+        products.Add(new Product("Виріб А", 2.5, 100, 8));
+        products.Add(new Product("Виріб В", 1.0, 150, 7));
+        products.Add(new Product("Виріб C", 3.0, 120, 9));
+
+        Console.WriteLine("Сортувати вироби за ціною:");
+        foreach (var product in products)
+        {
+            Console.WriteLine(product);
+        }
+
+        Console.WriteLine("\nСортування виробів за вагою:");
+        List<Product> productList = new List<Product>(products);
+        productList.Sort(); 
+        productList.ForEach(Console.WriteLine);
+
+        Console.WriteLine("\nСортування виробів за якістю:");
+        productList.Sort(new ProductQualityComparer());
+        productList.ForEach(Console.WriteLine);
+        Console.WriteLine("Натисніть будь-яку клавішу, щоб закрити програму...");
+        Console.ReadKey();
+    }
+}
